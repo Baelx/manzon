63b2577ec6edc71ebc526512a8c946cc0d778992
Alex <awintschel@gmail.com>
2019-06-27 13:56:57 -0700

    changed a bunch of styles, added smooth scroll

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import slick from 'slick-carousel';
+import smoothScroll from 'jquery-smooth-scroll';
 
 // Import local modules
 import WPHelper from './modules/Waypoints';
@@ -9,9 +10,10 @@ import seeMoreHover from './modules/Hover';
 // Instantiate new waypoints
 const ctaUnderline = new WPHelper($(".underline"), "70%", "show-underline");
 const featureUnderline = new WPHelper($(".underline--feature"), "80%", "show-underline");
+const checkMarks = new WPHelper($(".check"), "70%", "check--animate");
 
-const checkMarks = new WPHelper($(".check"), "80%", "check--animate");
-
+// Smooth Scrolling
+$('.hero__see-more-link').smoothScroll();
 
 // // Gallery sliders
 // const residentialSlider = new GallerySlider($('.gallery__residential'), $('.gallery__residential--nav'));
@@ -50,12 +52,14 @@ $('.slider-nav').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: '.slider-display',
-  arrows: true,
+  arrows: false,
   focusOnSelect: true,
   variableWidth: true,
   autoplay: true,
   autoplaySpeed: 4000,
   lazyLoad: 'progressive',
-  pauseOnFocus: true
-
+  pauseOnFocus: true,
+infinite: true
+  // nextArrow: '<i class="fa fa-arrow-right"></i>',
+  // prevArrow: '<i class="fa fa-arrow-left"></i>'
 });
